52626cfdfb09d2c8641614669ab917faf2c3332e
Tanay <tanaypatil210192@acropolis.in>
2024-11-09 06:49:14 +0530

    first commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,23 @@
+// import { SignIn } from "@clerk/nextjs";
+
+
+// export default function page() {
+//   return <SignIn />;
+// }
+
+
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+
+const SignInPage = () => {
+  return (
+    <SignIn
+      path="/sign-in"
+      routing="path"
+      fallbackRedirectUrl="/" // Redirect to home page after sign in
+    />
+  );
+};
+
+export default SignInPage;
